0a612befe412d3c9fd9ea0d14c3d2075f4581c8a
joaopaquino <https://github.com/joaopaquino/ProjectFeed.git>
2015-03-16 03:34:56 -0300

    Atualização de leitor

--- listaFeed/Controllers/FeedController.cs
+++ listaFeed/Controllers/FeedController.cs
@@ -1,20 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using listaFeed.Models;
 
 namespace listaFeed.Controllers
 {
     public class FeedController : Controller
     {
-        //
-        // GET: /Feed/
-
-        public ActionResult Index()
+        public ActionResult Feeds()
         {
-            return View();
+            List<Feed> feeds = new List<Feed>();            
+            List<SyndicationItem> feedItens = readFeed("http://www.minutoseguros.com.br/blog/feed/");
+
+            foreach (SyndicationItem item in feedItens)
+            {
+                Feed feed = new Feed();
+                feed.Titulo = item.Title.Text;
+                feed.Resumo = item.Summary.Text;
+                feed.Link = item.Links[0].Uri;
+
+                feeds.Add(feed);
+            }
+            return View(feeds);
         }
 
+        //lista ultimos 10 feeds
+        public List<SyndicationItem> readFeed(string urlBlogMinuto)
+        {
+            XmlReader readFeed = XmlReader.Create(urlBlogMinuto);
+            SyndicationFeed feed = SyndicationFeed.Load(readFeed);
+            IEnumerable<SyndicationItem> feedItens = feed.Items;
+            feedItens.Skip(10);
+
+            return feedItens.ToList();
+        }
     }
 }
--- listaFeed/Models/Feed.cs
+++ listaFeed/Models/Feed.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ServiceModel.Syndication;
+using System.Xml;
 
 namespace listaFeed.Models
 {
     public class Feed
     {
-        public void readFeed()
-        {
+        string titulo;
+        string resumo;
+        int numeroPalavras;
+        Uri link;
 
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }      
+
+        public int NumeroPalavras
+        {
+            get { return numeroPalavras; }
+            set { numeroPalavras = value; }
+        }       
+
+        public Uri Link
+        {
+            get { return link; }
+            set { link = value; }
+        }
+
+        public string Resumo
+        {
+            get { return resumo; }
+            set { resumo = value; }
         }
     }
 }
